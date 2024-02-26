263eeb6991ad6a724799a460f2237a88029fb746
alessiavozzo <a.vozzo95@gmail.com>
2024-02-26 12:21:01 +0100

    add jsnack6

---
+++ jssnack6/main.js
@@ -0,0 +1,11 @@
+/* Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente. */
+
+
+const userNumber = Number(prompt("Inserisci un numero"))
+console.log(userNumber**3);
+
+
+
+
+
